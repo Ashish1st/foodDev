0c319cfb64ec5138a5e67cb8ae69947a9e6b09e7
Ashish1st <branch9399@gmail.com>
2025-02-23 14:35:12 +0530

    spell change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
     >
       <div className="headerContent absolute flex flex-col items-start gap-[1.5vw] max-w-[65%] sm:max-w-[50%] bottom-[10%] left-[6vw]">
         <h2 className="font-semibold text-white text-[max(4.5vw,22px)] leading-[max(4.5vw,22px)]">
-          Bringing delicious meals to your doosteps ,
+          Bringing delicious meals to your doorsteps ,
         </h2>
         <p className="text-[1.3vw] text-white font-semibold leading-[1.3vw] hidden sm:block">
           because good food is always just a click away.
